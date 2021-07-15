049821bffe1bc60ca967095d6e8a17ad92e5893c
MarselFu <marselfu46@gmail.com>
2021-07-15 20:10:08 +0700

    P

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import MobileLegend from './img/Mobile_Legend.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Welcome To Mobile Legend </h1>
+        <img src={MobileLegend} classname="App-logo" alt="MobileLegend"/>
+        <h3>Marcellino Fadhilah Utomo</h3>
+        <h4>Kelompok Microsoft</h4>
+        <p>Mobile Legend is a 5 vs 5 strategy game created by Moonton</p>
+        <a href="https://m.mobilelegends.com/en">Go To Website Mobile Legend</a>
       </header>
     </div>
   );
